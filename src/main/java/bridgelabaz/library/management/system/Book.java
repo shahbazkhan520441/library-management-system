fd03d467017af905a34bcb7465745447f2e1df53
shahbazkhan520441 <shahbazkhan520441@gmail.com>
2024-09-04 11:54:48 +0530

    created book enity

--- src/main/java/bridgelabaz/library/management/system/Book.java
+++ src/main/java/bridgelabaz/library/management/system/Book.java
@@ -1,5 +1,21 @@
 package bridgelabaz.library.management.system;
 
 public class Book {
+	
+	private String isbn;
+	private String title;
+	private String author;
+	private boolean isAvailable;
+	
+	public Book(String isbn, String title, String author, boolean isAvailable) {
+		super();
+		this.isbn = isbn;
+		this.title = title;
+		this.author = author;
+		this.isAvailable = isAvailable;
+	}
+	
+	
+	
 
 }
